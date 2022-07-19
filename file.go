d793dce74f0afa6abdb0b5e05aee00eff1941224
naoto67 <uver.com67@gmail.com>
2022-07-19 22:49:48 +0900

    update

--- file.go
+++ file.go
@@ -12,8 +12,18 @@ func WriteFile(p string, data []byte) error {
 	if err := os.MkdirAll(dir, os.FileMode(0o755)); err != nil && !os.IsExist(err) {
 		return errors.Wrap(err, "dirの作成に失敗しました")
 	}
-
 	if err := os.WriteFile(p, data, os.FileMode(0o755)); err != nil {
+		return errors.Wrap(err, "fileの書き込みに失敗しました。")
+	}
+	return nil
+}
+
+func WriteFileWithFileMode(p string, data []byte, perm os.FileMode) error {
+	dir := path.Dir(p)
+	if err := os.MkdirAll(dir, os.FileMode(0o755)); err != nil && !os.IsExist(err) {
+		return errors.Wrap(err, "dirの作成に失敗しました")
+	}
+	if err := os.WriteFile(p, data, perm); err != nil {
 		return errors.Wrap(err, "fileの書き込みに失敗しました。")
 	}
 	return nil
