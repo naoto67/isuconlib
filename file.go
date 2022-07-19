34c8832c175c07a5e94eda1fadb125585e84f0d8
naoto67 <uver.com67@gmail.com>
2022-07-19 22:43:49 +0900

    fix

---
+++ file.go
@@ -0,0 +1,20 @@
+package isuconlib
+
+import (
+	"os"
+	"path"
+
+	"github.com/pkg/errors"
+)
+
+func WriteFile(p string, data []byte) error {
+	dir := path.Dir(p)
+	if err := os.MkdirAll(dir, os.FileMode(0o755)); err != nil && !os.IsExist(err) {
+		return errors.Wrap(err, "dirの作成に失敗しました")
+	}
+
+	if err := os.WriteFile(p, data, os.FileMode(0o755)); err != nil {
+		return errors.Wrap(err, "fileの書き込みに失敗しました。")
+	}
+	return nil
+}
